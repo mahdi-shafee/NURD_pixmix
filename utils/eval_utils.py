e7996aeb656ac247bd68944bf30fe44663eb357f
mahdi-shafee <82397556+mahdi-shafee@users.noreply.github.com>
2023-08-19 01:41:03 +0330

    Update eval_utils.py

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -47,7 +47,7 @@ def get_dataloaders(args, exact=False):
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
-        testsetout = NuisanceDataWrapper(root="/places/ood1/placesbg",
+        testsetout = NuisanceDataWrapper(root="content/Spurious_OOD/datasets/placesbg",
                                     transform=large_transform)
         testloaderOut = torch.utils.data.DataLoader(testsetout, batch_size=args.batch_size, shuffle=False,
                                                     num_workers=multiprocessing.cpu_count())
